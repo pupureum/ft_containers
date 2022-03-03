cd763434b7053d30b85d8b9893574077b1a91cf2
pureum <dlvnfma0501@naver.com>
2022-03-04 00:53:28 +0900

    start

---
+++ vector.hpp
@@ -0,0 +1,26 @@
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+namespace ft
+{
+	template <class T, class Alloc = std::allocator<T> >
+	class vector
+	{
+	private:
+		/* data */
+	public:
+		vector(/* args */);
+		~vector();
+	};
+	
+	vector::vector(/* args */)
+	{
+	}
+	
+	vector::~vector()
+	{
+	}
+	
+}
+
+#endif
