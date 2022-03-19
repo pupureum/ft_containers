7bfb1b71bdd224c31e1e0ca17af6e559f3039f9a
pureum <dlvnfma0501@naver.com>
2022-03-19 22:17:50 +0900

    ADD vector_iterator

--- iterator.hpp
+++ iterator.hpp
@@ -1,7 +1,7 @@
 #ifndef ITERATOR_HPP
 # define ITERATOR_HPP
 
-#include <cstddef>
+# include <cstddef>
 
 namespace ft
 {
@@ -65,6 +65,7 @@ namespace ft
 		typedef typename T&								reference;
 	};
 
+
 	/*####################*/
 	/*# Reverse Iterator #*/
 	/*####################*/
@@ -89,7 +90,7 @@ namespace ft
 
 	public:
 			/* Constructor */
-			reverse_iterator() : _current() {}
+			reverse_iterator(void) : _current() {}
 			explicit reverse_iterator(iterator_type it) : _current(it) {}
 			//check
 			template <class Iter>
@@ -97,29 +98,30 @@ namespace ft
 
 			/* Assignment Operator */
 			template <class Iter>
-			reverse_iterator	&operator=(const reverse_iterator<Iter>& rhs)
+			reverse_iterator& operator=(const reverse_iterator<Iter>& rhs)
 			{
-				this->_current = rhs._current;
+				if (this != &rhs)
+					this->_current = rhs._current;
 				return (*this);
 			}
 
 			/* Destrcutor */
-			~reverse_iterator() {}
+			~reverse_iterator(void) {}
 
 			/* Member function */
-			iterator_type base() const { return (this->_current); }
+			iterator_type base(void) const { return (this->_current); }
 
 			/* Member function Operators 정방향 반복자는 가리키는 원소의 value를 참조하지만 역방향 반복자는 가리키는 다음 원소의 value를 참조한다*/ 
-			reference operator*() const { Iterator temp = _current; return (*--temp); }
-			pointer operator->() const { return (&(operator*())); }
-			reverse_iterator& operator++() { --_current; return (*this); }
-			reverse_iterator operator++(int) { reverse_iterator temp(*this); ++(*this); return (temp); } // 윗줄에서 만들어놓은 ++이 호출되어 연산되므로 ++ 해준다!
-			reverse_iterator operator--() { ++_current; return (*this); }
-			reverse_iterator operator--(int) { reverse_iterator temp(*this); --(*this); return(temp); }
+			reference operator*(void) const { iterator_type temp = _current; return (*(--temp)); }
+			pointer operator->(void) const { return (&(this->operator*())); }
 			reverse_iterator operator+(difference_type n) const { return (reverse_iterator(_current - n)); }
-			reverse_iterator operator+=(difference_type n) { _current -= n; return (*this); }
+			reverse_iterator& operator++(void) { --_current; return (*this); }
+			reverse_iterator operator++(int) { reverse_iterator temp(*this); ++(*this); return (temp); } // 윗줄에서 만들어놓은 ++이 호출되어 연산되므로 ++ 해준다!
 			reverse_iterator operator-(difference_type n) const { return (reverse_iterator(_current + n)); }
-			reverse_iterator operator-=(difference_type n) { _current += n; return (*this); }
+			reverse_iterator& operator--(void) { ++_current; return (*this); }
+			reverse_iterator operator--(int) { reverse_iterator temp(*this); --(*this); return(temp); }
+			reverse_iterator& operator+=(difference_type n) { _current -= n; return (*this); }
+			reverse_iterator& operator-=(difference_type n) { _current += n; return (*this); }
 			reference operator[](difference_type n) const { return (*this + n); }
 			
 	};
@@ -155,105 +157,46 @@ namespace ft
 	//이 호출되지만! temp = 2 + temp; 와 같이 숫자 먼저 나오고 + 이터레이터 인 경우를 위해 필요.
 	//매개변수 보면 difference_type 먼저 받고 이터레이터 들어온다. 그러면 기존의 멤버함수 오버로딩+가 다시 호출되어 연산.
 	reverse_iterator<Iterator> operator+(
-			typename reverse_iterator<Iterator>::difference_type n, const reverse_iterator<Iterator>& rev_it)
-	{ return reverse_iterator<Iterator>(rev_it.base() + n); }
+			typename reverse_iterator<Iterator>::difference_type n, const reverse_iterator<Iterator>& rev_iter)
+	{ return (reverse_iterator<Iterator>(rev_iter.base() + n)); }
 
 	template <typename Iterator>
-	typename reverse_iterator<Iterator>::difference_type operator- (
+	typename reverse_iterator<Iterator>::difference_type operator-(
 			const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs)
-	{ return rhs.base() - lhs.base(); }
+	{ return (rhs.base() - lhs.base()); }
 
-	/*###################*/
-	/*# Vector Iterator #*/
-	/*###################*/
 
-	template <typename Iterator>
-	class vector_iterator
-	{
-	public:
-		typedef Iterator													iterator_type;
-		typedef typename iterator_traits<iterator_type>::iterator_category	iterator_category;
-		typedef typename iterator_traits<iterator_type>::value_type			value_type;
-		typedef typename iterator_traits<iterator_type>::difference_type	difference_type;
-		typedef typename iterator_traits<iterator_type>::pointer			pointer;
-		typedef typename iterator_traits<iterator_type>::reference			reference;
-	
-	protected:
-		iterator_type _i;
-	
-	public:
-		/* Constructor */
-		vector_iterator() : _i() {}
-		vector_iterator(const vector_iterator& vec_it) : _i(vec_it.base()) {}
-		vector_iterator(iterator_type it) : _i(it) {}
-		template <class Iter>vector_iterator(const vector_iterator<Iter>& vec_iter) : _i(vec_iter.base()) {}
-	
-		/* Assignment Operator */
-		vector_iterator& operator=(const vector_iterator& vec_rhs)
-		{
-			this->_current = vec_rhs._current;
-			return (*this);
-		}
 
-		/* Destrcutor */
-		~vector_iterator() {}
+// template <typename InputIterator>
+// typename iterator_traits<InputIterator>::difference_type __distance(
+//     InputIterator first,
+//     InputIterator last,
+//     ft::input_iterator_tag) {
+//   typename ft::iterator_traits<InputIterator>::difference_type len(0);
 
-		/* Member function */
-		iterator_type base() const { return (_i); }
+//   for (;first != last; ++first)
+//     ++len;
+//   return len;
+// }
 
-		/* Member function Operators */
-		reference operator*() const { return (*_i); }
-		pointer operator->() const { return &(this->operator*()); }
-		vector_iterator& operator++() { ++_i; return (*this); }
-		vector_iterator operator++(int) { vector_iterator temp(*this); ++(*this); return (temp); }
-		vector_iterator& operator--() { --_i; return (*this); }
-		vector_iterator operator--(int) { vector_iterator temp(*this); --(*this); return (temp); }
-		vector_iterator operator+(difference_type n) const { return (vector_iterator((this->_i) + n); }
-		vector_iterator operator+=(difference_type n) { _i += n; return (*this); }
-		vector_iterator operator-(difference_type n) { return (vector_iterator((this->_i) - n); }
-		vector_iterator operator-=(difference_type n) { *this += -n; return (*this); }
-		reference operator[](difference_type n) const { return (i[n]); }
-	};
+// template <typename InputIterator>
+// typename iterator_traits<InputIterator>::difference_type __distance(
+//     InputIterator first,
+//     InputIterator last,
+//     ft::random_access_iterator_tag) {
+//   return last - first;
+// }
 
-	/*random은 vector안에서 iterator 위치끼리 같은지 비교, + - 연산은 reverse처럼 data + iterator 인 경우를 위해*/
-	/*Relational operators*/
-	template <typename Iter1, typename Iter2>
-	bool operator==(const reverse_iterator<Iter1>& lhs, const reverse_iterator<Iter2>& rhs)
-	{ return (lhs.base() == rhs.base()); }
+// template <typename InputIterator>
+// typename iterator_traits<InputIterator>::difference_type distance(
+//     InputIterator first, InputIterator last) {
+//   return ft::__distance(
+//     first,
+//     last,
+//     typename ft::iterator_traits<InputIterator>::iterator_category());
+// }
 
-	template <typename Iter1, typename Iter2>
-	bool operator!=(const reverse_iterator<Iter1>& lhs, const reverse_iterator<Iter2>& rhs)
-	{ return (lhs.base() != rhs.base()); }
 
-	template <typename Iter1, typename Iter2>
-	bool operator<(const reverse_iterator<Iter1>& lhs, const reverse_iterator<Iter2>& rhs)
-	{ return !(lhs.base() < rhs.base()); }
-
-	template <typename Iter1, typename Iter2>
-	bool operator<=(const reverse_iterator<Iter1>& lhs, const reverse_iterator<Iter2>& rhs)
-	{ return !(lhs.base() <= rhs.base()); }
-
-	template <typename Iter1, typename Iter2>
-	bool operator>(const reverse_iterator<Iter1>& lhs, const reverse_iterator<Iter2>& rhs)
-	{ return !(lhs.base() > rhs.base()); }
-	
-	template <typename Iter1, typename Iter2>
-	bool operator>=(const reverse_iterator<Iter1>& lhs, const reverse_iterator<Iter2>& rhs)
-	{ return !(lhs.base() >= rhs.base()); }
-
-	template <typename Iter>
-	// vector_iterator(random_access)는 vec안에 iter위치끼리 비교!
-	vector_iterator<Iter> operator+(
-			typename vector_iterator<Iter>::difference_type n, const vector_iterator<Iter>& v_it)
-	{ return (v_it + n); }
-
-	template <typename Iter1, typename Iter2>
-	typename vector_iterator<Iter1>::difference_type operator- (
-			const vector_iterator<Iter1>& lhs, const vector_iterator<Iter2>& rhs)
-	{ return lhs.base() - rhs.base(); }
-
-//한쪽은 const형 iter 한쪽은 그냥 iter알때 를 위해서 iter1, iter2 나눔! 
-//+는 숫자와 iter 더할 수 있지만 -는 iter끼리만 가능하므로!
-}
+} //namespace ft
 
 #endif
---
+++ random_access_iterator.hpp
@@ -0,0 +1,98 @@
+#ifndef RANDOMACCESS_ITERATOR_HPP
+# define RANDOMACCESS_ITERATOR_HPP
+
+# include "iterator.hpp"
+
+namespace ft
+{
+	template <typename T>
+	class random_access_iterator : public iterator<random_access_iterator_tag, T>
+	{
+	public:
+		typedef typename iterator<random_access_iterator_tag, T>::iterator_category	iterator_category;
+		typedef typename iterator<random_access_iterator_tag, T>::value_type		value_type;
+		typedef typename iterator<random_access_iterator_tag, T>::difference_type	difference_type;
+		typedef typename iterator<random_access_iterator_tag, T>::pointer			pointer;
+		typedef typename iterator<random_access_iterator_tag, T>::reference			reference;
+	
+	protected:
+		pointer _i;
+	
+	public:
+		/* Constructor */
+		random_access_iterator() : _i(NULL) {}
+		//?
+		explicit random_access_iterator(pointer ptr = ft_nullptr) : _i(ptr) {}
+		random_access_iterator(const random_access_iterator<T>& vec_rhs) : _i(vec_rhs._i) {}
+	
+		/* Assignment Operator */
+		random_access_iterator& operator=(const random_access_iterator& vec_rhs)
+		{
+			if (this != &vec_rhs)
+				this->_current = vec_rhs._current;
+			return (*this);
+		}
+
+		/* Destrcutor */
+		~random_access_iterator(void) {}
+
+		/* Member function */
+		pointer base(void) const { return (_i); }
+
+		/* Member function Operators */
+		reference operator*() const { return (*_i); }
+		pointer operator->() const { return &(this->operator*()); }
+		random_access_iterator operator+(difference_type n) const { return (random_access_iterator(this->_i + n); }
+		random_access_iterator& operator++(void) { ++_i; return (*this); }
+		random_access_iterator operator++(int) { random_access_iterator temp(*this); ++_i; return (temp); }
+		random_access_iterator operator-(difference_type n) { return (random_access_iterator(this->_i - n); }
+		random_access_iterator& operator--(void) { --_i; return (*this); }
+		random_access_iterator operator--(int) { random_access_iterator temp(*this); --_i; return (temp); }
+		random_access_iterator& operator+=(difference_type n) { this->_i += n; return (*this); }
+		random_access_iterator& operator-=(difference_type n) { this->_i -= n; return (*this); }
+		reference operator[](difference_type n) const { return (_i[n]); }
+	};
+
+	/*random은 vector안에서 iterator 위치끼리 같은지 비교, + - 연산은 reverse처럼 data + iterator 인 경우를 위해*/
+	/*Relational operators*/
+	template <typename Iter1, typename Iter2>
+	bool operator==(const random_access_iterator<Iter1>& lhs, const random_access_iterator<Iter2>& rhs)
+	{ return (lhs.base() == rhs.base()); }
+
+	template <typename Iter1, typename Iter2>
+	bool operator!=(const random_access_iterator<Iter1>& lhs, const random_access_iterator<Iter2>& rhs)
+	{ return (lhs.base() != rhs.base()); }
+
+	template <typename Iter1, typename Iter2>
+	bool operator<(const random_access_iterator<Iter1>& lhs, const random_access_iterator<Iter2>& rhs)
+	{ return !(lhs.base() < rhs.base()); }
+
+	template <typename Iter1, typename Iter2>
+	bool operator<=(const random_access_iterator<Iter1>& lhs, const random_access_iterator<Iter2>& rhs)
+	{ return !(lhs.base() <= rhs.base()); }
+
+	template <typename Iter1, typename Iter2>
+	bool operator>(const random_access_iterator<Iter1>& lhs, const random_access_iterator<Iter2>& rhs)
+	{ return !(lhs.base() > rhs.base()); }
+	
+	template <typename Iter1, typename Iter2>
+	bool operator>=(const random_access_iterator<Iter1>& lhs, const random_access_iterator<Iter2>& rhs)
+	{ return !(lhs.base() >= rhs.base()); }
+
+	template <typename Iter>
+	// random_access_iterator(random_access)는 vec안에 iter위치끼리 비교!
+	random_access_iterator<Iter> operator+(
+			typename random_access_iterator<Iter>::difference_type n, const random_access_iterator<Iter>& iter)
+	{ return (iter + n); }
+
+	template <typename Iter1, typename Iter2>
+	typename random_access_iterator<Iter1>::difference_type operator-(
+			const random_access_iterator<Iter1>& lhs, const random_access_iterator<Iter2>& rhs)
+	{ return lhs.base() - rhs.base(); }
+
+//한쪽은 const형 iter 한쪽은 그냥 iter알때 를 위해서 iter1, iter2 나눔! 
+//+는 숫자와 iter 더할 수 있지만 -는 iter끼리만 가능하므로!
+
+}
+
+#endif
